a8ddbe84c1bb3424ece31e1158ae678cc3a0f094
Souritra-Garai <souritra.garai@gmail.com>
2022-05-13 21:35:54 +0530

    Modified script to save figures.

--- scripts/measurements/flame_speed.py
+++ scripts/measurements/flame_speed.py
@@ -35,14 +35,14 @@ flame_front_temperature = 1000	# K
 
 flame_locations = x[np.argmin(np.abs(T - flame_front_temperature), axis=1)]	# m
 
-valid_indices = np.where(flame_locations[1:] > flame_locations[:-1])
+valid_indices = np.where(flame_locations != 0.0)
 # print(flame_locations[valid_indices])
 # print(flame_locations[valid_indices].shape)
 
 flame_locations = flame_locations[valid_indices]
 t = t[valid_indices]
 
-valid_indices = np.where(t < 0.62)
+valid_indices = np.where(np.logical_and(t > 0.0, t < 0.64))
 
 flame_locations = flame_locations[valid_indices]
 t = t[valid_indices]
@@ -52,25 +52,25 @@ line, cov = np.polyfit(t, flame_locations, 1, cov=True)
 
 print('Flame Speed : ', line[0]*1000, ' mm/s')
 
-fig = plt.figure()
+fig = plt.figure(figsize=[4*1.6, 4], constrained_layout=True)
 ax = fig.add_subplot()
 
-ax.plot(t, flame_locations, label='Simulation Results')
-ax.plot(t, np.polyval(line, t), label='Line fit', lw=1)
+ax.plot(t, flame_locations * 1E3, label='Simulation Results')
+ax.plot(t, np.polyval(line, t) * 1E3, label='Linear fit : ' + '$x = ' + str(round(line[0] * 1E3, 4)) + '\;t + ' + str(round(line[1] * 1E3, 4)) + '$', lw=1)
 
-ax.set_title('Flame Speed : ' +  str(line[0]*100) + ' cm/s')
+# ax.set_title('Flame Speed : ' +  str(line[0]*100) + ' cm/s')
 
-ax.set_xlabel('Time (seconds)')
-ax.set_ylabel('Combustion Front Location')
+ax.set_xlabel('Time, $t$ (seconds)')
+ax.set_ylabel('Combustion Front Location, $x$ (mm)')
 
-ax.grid(which='major', color='grey')
+ax.grid(which='major', color='lightgrey')
 ax.minorticks_on()
-ax.grid(which='minor', color='grey', ls='--')
+ax.grid(which='minor', color='lightgrey', ls='--')
 
 ax.legend()
 
 # fig.set_size_inches(10, 4.5)
 # fig.set_dpi(600)
 
-# plt.savefig(folder + '/Flame_Position_2000.png', dpi=600)
+# plt.savefig(folder + '/Flame Position.png', dpi=600, transparent=True)
 plt.show()
--- scripts/visualization/temperature_2D_line_plot.py
+++ scripts/visualization/temperature_2D_line_plot.py
@@ -27,7 +27,7 @@ print('Processing solution at the directory ' + folder)
 data = np.genfromtxt(os.path.join(folder, 'temperature.csv'), delimiter=',')
 
 t = data[1:, 0]
-x = data[0, 1:]
+x = data[0, 1:] * 1E3
 
 T = data[1:, 1:]
 
@@ -36,25 +36,30 @@ N = t.shape[0]
 # Number of line plots
 n = 10
 
-fig = plt.figure()
+fig = plt.figure(figsize=[4*1.6, 4], constrained_layout=True)
 
 line_plot_axes = fig.add_subplot(1, 1, 1)
 
 for i in range(n) :
 
-	line_plot_axes.plot(x, T[i * N // n], label=u'Time t = {:.3f} seconds'.format(t[i * N // n]))
+	line_plot_axes.plot(x, T[i * N // n], label=u'$t$ = {:.0f} ms'.format(t[i * N // n] * 1E3))
+	# line_plot_axes.plot([], [], label=u't = {:.0f} ms'.format(t[i * N // n] * 1E3))
 
-line_plot_axes.plot(x, T[-1], label=u"Time t = {:.3f} seconds".format(t[-1]))
+line_plot_axes.plot(x, T[-1], label=u"$t$ = {:.0f} ms".format(t[-1] * 1E3))
+# line_plot_axes.plot([], [], label=u"t = {:.0f} ms".format(t[-1] * 1E3))
 
-line_plot_axes.set_xlabel('x (m)')
-line_plot_axes.set_ylabel('T (K)')
+line_plot_axes.set_xlabel('$x$ (mm)')
+line_plot_axes.set_ylabel('Temperature (K)')
 
-line_plot_axes.set_title('Temperature Evolution in Pellet')
+# line_plot_axes.set_title('Temperature Evolution in Pellet')
 
-line_plot_axes.grid(which='major', color='grey')
+line_plot_axes.grid(which='major', color='lightgrey')
 line_plot_axes.minorticks_on()
-line_plot_axes.grid(which='minor', color='grey', ls='--')
+line_plot_axes.grid(which='minor', color='lightgrey', ls='--')
 
+# line_plot_axes.set_axis_off()
 line_plot_axes.legend()
 
 plt.show()
+# plt.savefig(folder + '/flame_propagation.png', dpi=600, transparent=True)
+# plt.savefig(folder + '/flame_propagation_legend.png', dpi=600, transparent=True)
