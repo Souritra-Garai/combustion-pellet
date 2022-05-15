00adcd195ce0cf21659cf13ac7dd5a5c0dfbff36
Souritra-Garai <souritra.garai@gmail.com>
2022-05-15 23:55:49 +0530

    Modified diffusivity model to accomodate lower temperatures.

--- include/thermo-physical-properties/Arrhenius_Diffusivity_Model.cuh
+++ include/thermo-physical-properties/Arrhenius_Diffusivity_Model.cuh
@@ -5,6 +5,9 @@ class ArrheniusDiffusivityModel
 {
     private:
 
+		double _pre_exponential_factor_low;
+		double _activation_energy_low;
+
         double _pre_exponential_factor;
         double _activation_energy;
 
@@ -15,10 +18,12 @@ class ArrheniusDiffusivityModel
             double activation_energy
         ) {
             setParameters(pre_exponential_factor, activation_energy);
+			setParametersLow(0, 0);
         }
 
         __host__ __device__ ArrheniusDiffusivityModel() {
             setParameters(0, 0);
+			setParametersLow(0, 0);
         }
 
         __host__ __device__ void setParameters(
@@ -28,17 +33,24 @@ class ArrheniusDiffusivityModel
             _pre_exponential_factor = pre_exponential_factor;
             _activation_energy = activation_energy;
         }
-		
-        __host__ __device__ double getDiffusivity(double temperature) {
-            return _pre_exponential_factor * exp(- _activation_energy / (8.314 * temperature));
-        }
 
-        __host__ __device__ double getPreExponentialFactor() {
-            return _pre_exponential_factor;
+		__host__ __device__ void setParametersLow(
+			double pre_exponential_factor,
+            double activation_energy
+        ) {
+            _pre_exponential_factor_low = pre_exponential_factor;
+            _activation_energy_low = activation_energy;
         }
 		
-        __host__ __device__ double getActivationEnergy() {
-            return _activation_energy;
+        inline __host__ __device__ double getDiffusivity(double temperature) {
+			
+			if (temperature >= 933.0)
+
+            	return _pre_exponential_factor * exp(- _activation_energy / (8.314 * temperature));
+
+			else
+
+				return _pre_exponential_factor_low * exp(- _activation_energy_low / (8.314 * temperature));
         }
 };
 
